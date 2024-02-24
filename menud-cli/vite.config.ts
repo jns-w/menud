cd2ee2d2d77ee5d819b308456987352b74006e8a
jns-w <jns.w@icloud.com>
2024-02-24 19:48:28 +0800

    production config

--- menud-cli/vite.config.ts
+++ menud-cli/vite.config.ts
@@ -1,23 +1,17 @@
-import {defineConfig, loadEnv} from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-export default defineConfig(({mode, command}) => {
-  const env = loadEnv(mode, process.cwd(), "")
-  return {
-    define: {
-      "process.env.VITE_API_ENDPOINT": env.VITE_API_ENDPOINT,
-    },
-    base: "/",
-    plugins: [react()],
-    preview: {
-      port: 80,
-      strictPort: true,
-    },
-    server: {
-      port: 80,
-      strictPort: true,
-      host: true,
-      origin: "http://0.0.0.0:8080",
-    },
-  }
+export default defineConfig({
+  base: "/",
+  plugins: [react()],
+  preview: {
+    port: 80,
+    strictPort: true,
+  },
+  server: {
+    port: 80,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8080",
+  },
 });
